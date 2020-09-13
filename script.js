1579360548c8ce0f73f5a2b71c13bf943187e99a
Mongolmaa Gurbazar <mongolmaa555@gmail.com>
2020-09-13 16:31:14 -0400

    localStorage

--- script.js
+++ script.js
@@ -1,65 +1,69 @@
-var dayPlanner = JSON.parse(localStorage.getItem("dayPlanner")) || {};
-var localStorageObj = localStorage.getItem("dayPlanner");
+$(document).ready(function () {
+  var dayPlanner = JSON.parse(localStorage.getItem("dayPlanner")) || {};
 
-var test = false;
-$(".saveBtn").on("click", function (e) {
-  var storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
-  if (test) {
-    console.log(storedPlans);
+  var test = true;
+  $(".saveBtn").on("click", function (e) {
+    var storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
+    // if (!test) {
+    //   console.log(storedPlans);
+    // }
+
+    if (storedPlans !== null) {
+      planTextArr = storedPlans;
+    } else {
+      //this should only occur on first time the app is loaded in the browser
+      planTextArr = new Array(9);
+      planTextArr[4] = "Exercise";
+    }
+
+    if (!test) {
+      console.log("full array of plned text", planTextArr);
+    }
+
+    var id = $(this).attr("id");
+    var task = $(this).siblings(".description").children("textarea").val();
+
+    // if (test) {
+    //   console.log(task);
+    // }
+    dayPlanner[id] = task;
+    localStorage.setItem("dayPlanner", JSON.stringify(dayPlanner));
+  });
+
+  var currentDay = moment().format("MMMM Do YYYY");
+  $("#currentDay").text(currentDay);
+
+  // var colorOfTime = [];
+
+  function hourTracker() {
+    //get current number of hours
+    var currentHour = moment().format("H");
+
+    //loop over time blocks
+    $(".description").each(function () {
+      var blockHour = $(this).attr("id");
+      // console.log("Here is $(this.attr('id'): ", $(this).attr("id"));
+      // console.log("blockHour: ", blockHour);
+      // console.log("currentHour", currentHour);
+      console.log(blockHour, currentHour);
+
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+      } else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+      } else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+      }
+    });
   }
 
-  // if (storedPlans !== null) {
-  //   planTextArr = storedPlans;
-  // } else {
-  //   //this should only occur on first time the app is loaded in the browser
-  //   planTextArr = new Array(9);
-  //   planTextArr[4] = "Exercise";
-  // }
-
-  // if (test) {
-  //   console.log("full array of plned text", planTextArr);
-  // }
-
-  var id = $(this).attr("id");
-  var task = $(this).siblings(".description").children("textarea").val();
-
-  // if (test) {
-  //   console.log(task);
-  // }
-  dayPlanner[id] = task;
-  localStorage.setItem("dayPlanner", JSON.stringify(dayPlanner));
+  $("#09").val(localStorage.getItem("9am"));
+  // $(task).val(localStorage.setItem("id"));
+  hourTracker();
 });
-
-var currentDay = moment().format("MMMM Do YYYY");
-$("#currentDay").text(currentDay);
-
-// var colorOfTime = [];
-
-function hourTracker() {
-  //get current number of hours
-  var currentHour = moment().format("H");
-
-  //loop over time blocks
-  $(".description").each(function () {
-    var blockHour = $(this).attr("id");
-    // console.log("Here is $(this.attr('id'): ", $(this).attr("id"));
-    // console.log("blockHour: ", blockHour);
-    // console.log("currentHour", currentHour);
-    console.log(blockHour, currentHour);
-
-    if (blockHour < currentHour) {
-      $(this).addClass("past");
-      $(this).removeClass("future");
-      $(this).removeClass("present");
-    } else if (blockHour === currentHour) {
-      $(this).removeClass("past");
-      $(this).addClass("present");
-      $(this).removeClass("future");
-    } else {
-      $(this).removeClass("present");
-      $(this).removeClass("past");
-      $(this).addClass("future");
-    }
-  });
-}
-hourTracker();
