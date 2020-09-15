44adfc23363be253f3a5fa60a149315a3364db45
Mongolmaa Gurbazar <mongolmaa555@gmail.com>
2020-09-15 13:34:05 -0400

    wrote read me file, cleaned code, and saved events persisit even after the page refreshed

---
+++ README.md
@@ -0,0 +1,8 @@
+# Work Day Scheduler
+
+This is a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+I used [Moment.js](https://momentjs.com/) library to work with date and time.
+
+- The URL of the deployed application.
+- The URL of the GitHub repository
--- script.js
+++ script.js
@@ -1,40 +1,19 @@
 $(document).ready(function () {
-  var dayPlanner = JSON.parse(localStorage.getItem("dayPlanner")) || {};
-
-  var test = true;
+  //save button event
   $(".saveBtn").on("click", function (e) {
-    var storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
-    // if (!test) {
-    //   console.log(storedPlans);
-    // }
-
-    if (storedPlans !== null) {
-      planTextArr = storedPlans;
-    } else {
-      //this should only occur on first time the app is loaded in the browser
-      planTextArr = new Array(9);
-      planTextArr[4] = "Exercise";
-    }
-
-    if (!test) {
-      console.log("full array of plned text", planTextArr);
-    }
-
+    //selecting activite by id and assigning it to the var id
     var id = $(this).attr("id");
+    //assigning value of the activity to var task
     var task = $(this).siblings(".description").children("textarea").val();
-
-    // if (test) {
-    //   console.log(task);
-    // }
-    dayPlanner[id] = task;
-    localStorage.setItem("dayPlanner", JSON.stringify(dayPlanner));
+    //saving it to local storage
+    localStorage.setItem(id, task);
   });
-
+  // get date from moment.js
   var currentDay = moment().format("MMMM Do YYYY");
+  //writing current date by selecting element of html id
   $("#currentDay").text(currentDay);
 
-  // var colorOfTime = [];
-
+  //tracking hours by color
   function hourTracker() {
     //get current number of hours
     var currentHour = moment().format("H");
@@ -42,10 +21,7 @@ $(document).ready(function () {
     //loop over time blocks
     $(".description").each(function () {
       var blockHour = $(this).attr("id");
-      // console.log("Here is $(this.attr('id'): ", $(this).attr("id"));
-      // console.log("blockHour: ", blockHour);
-      // console.log("currentHour", currentHour);
-      console.log(blockHour, currentHour);
+      // console.log(blockHour, currentHour);
 
       if (blockHour < currentHour) {
         $(this).addClass("past");
@@ -61,12 +37,18 @@ $(document).ready(function () {
         $(this).addClass("future");
       }
     });
-
-var populateDesc={9:[], }
-
-    $("#09").val(localStorage.getItem("9am"));
-    $(task).val(localStorage.setItem("id"));
   }
+
+  //giving value to each block by their ids & element
+  $("#09").children("textarea").val(localStorage.getItem("hour-9"));
+  $("#10").children("textarea").val(localStorage.getItem("hour-10"));
+  $("#11").children("textarea").val(localStorage.getItem("hour-11"));
+  $("#12").children("textarea").val(localStorage.getItem("hour-12"));
+  $("#13").children("textarea").val(localStorage.getItem("hour-13"));
+  $("#14").children("textarea").val(localStorage.getItem("hour-14"));
+  $("#15").children("textarea").val(localStorage.getItem("hour-15"));
+  $("#16").children("textarea").val(localStorage.getItem("hour-16"));
+  $("#17").children("textarea").val(localStorage.getItem("hour-17"));
 
   hourTracker();
 });
